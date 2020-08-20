a801a66b24531a4749360cf86807c28cce580884
scx <282074579@qq.com>
2020-08-20 14:28:43 +0800

    ss

---
+++ dist/src/log4js/begin.js
@@ -0,0 +1,19 @@
+"use strict";
+let log4js = require("log4js");
+let logger = log4js.getLogger();
+logger.level = 'debug';
+logger.debug("这里是log4js的初始使用");
+// const log4js = require("log4js");
+// log4js.configure({
+//     appenders: { cheese: { type: "file", filename: "cheese.log" } },
+//     categories: { default: { appenders: ["cheese"], level: "error" } }
+// });
+//
+// const logger = log4js.getLogger("cheese");
+// logger.trace("Entering cheese testing");
+// logger.debug("Got cheese.");
+// logger.info("Cheese is Comté.");
+// logger.warn("Cheese is quite smelly.");
+// logger.error("Cheese is too ripe!");
+// logger.fatal("Cheese was breeding ground for listeria.");
+//# sourceMappingURL=begin.js.map
