b5a48ce317ac9591c817b359625c1abb41fdfa8f
scx <282074579@qq.com>
2020-08-25 11:45:38 +0800

    api3

---
+++ dist/src/api/net_client.js
@@ -0,0 +1,23 @@
+"use strict";
+// var net = require('net');
+//
+// var PORT = 3000;
+// var HOST = '127.0.0.1';
+//
+// // tcp客户端
+// var client = net.createConnection(PORT, HOST);
+//
+// client.on('connect', function(){
+//     console.log('客户端：已经与服务端建立连接');
+// });
+//
+// client.on('data', function(data){
+//     console.log('客户端：收到服务端数据，内容为{'+ data +'}');
+// });
+//
+// client.on('close', function(data){
+//     console.log('客户端：连接断开');
+// });
+//
+// // client.end('你好，我是客户端');
+//# sourceMappingURL=net_client.js.map
