df1bb7605f16578b3edb5c7be5bfafc8b0a1cee0
scx <282074579@qq.com>
2020-08-26 17:51:11 +0800

    api4

--- dist/src/api/path.js
+++ dist/src/api/path.js
@@ -1,17 +1,20 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
-console.log(path_1.default.basename('./http.js'));
-console.log(path_1.default.dirname('./http.js'));
-console.log(path_1.default.extname('./http.js'));
-console.log(path_1.default.basename('./http.js', path_1.default.extname("./http.js")));
-const name = 'joe';
-console.log(path_1.default.join('/', 'users', name, 'notes.txt')); //'/users/joe/notes.txt'
-console.log(path_1.default.join('/', 'test', 'finish', 'last.js'));
-console.log(path_1.default.resolve('/')); // 获取某个路径的绝对路径
-console.log(path_1.default.normalize('/users/joe/..//test.txt')); ///users/test.txt // 计算实际想表示的路径
-console.log(path_1.default.resolve('/users/joe/..//test.txt')); ///users/test.txt
+// import path from "path";
+//
+// console.log(path.basename('./http.js'));
+// console.log(path.dirname('./http.js'));
+// console.log(path.extname('./http.js'));
+// console.log(path.basename('./http.js',path.extname("./http.js")));
+// const name = 'joe';
+// console.log(path.join('/', 'users', name, 'notes.txt')); //'/users/joe/notes.txt'
+// console.log(path.join('/','test','finish','last.js'));
+//
+// console.log(path.resolve('/')); // 获取某个路径的绝对路径
+//
+// console.log(path.normalize('/users/joe/..//test.txt')); ///users/test.txt // 计算实际想表示的路径
+//
+// console.log(path.resolve('/users/joe/..//test.txt')); ///users/test.txt
+process.nextTick(function () {
+    console.log('nexttick');
+});
 //# sourceMappingURL=path.js.map
--- src/api/path.ts
+++ src/api/path.ts
@@ -1,16 +1,19 @@
-import path from "path";
+// import path from "path";
+//
+// console.log(path.basename('./http.js'));
+// console.log(path.dirname('./http.js'));
+// console.log(path.extname('./http.js'));
+// console.log(path.basename('./http.js',path.extname("./http.js")));
+// const name = 'joe';
+// console.log(path.join('/', 'users', name, 'notes.txt')); //'/users/joe/notes.txt'
+// console.log(path.join('/','test','finish','last.js'));
+//
+// console.log(path.resolve('/')); // 获取某个路径的绝对路径
+//
+// console.log(path.normalize('/users/joe/..//test.txt')); ///users/test.txt // 计算实际想表示的路径
+//
+// console.log(path.resolve('/users/joe/..//test.txt')); ///users/test.txt
 
-console.log(path.basename('./http.js'));
-console.log(path.dirname('./http.js'));
-console.log(path.extname('./http.js'));
-console.log(path.basename('./http.js',path.extname("./http.js")));
-const name = 'joe';
-console.log(path.join('/', 'users', name, 'notes.txt')); //'/users/joe/notes.txt'
-console.log(path.join('/','test','finish','last.js'));
-
-console.log(path.resolve('/')); // 获取某个路径的绝对路径
-
-console.log(path.normalize('/users/joe/..//test.txt')); ///users/test.txt // 计算实际想表示的路径
-
-console.log(path.resolve('/users/joe/..//test.txt')); ///users/test.txt
-
+process.nextTick(function(){
+    console.log('nexttick')
+})
